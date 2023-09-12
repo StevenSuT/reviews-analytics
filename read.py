f12cf84fb726da1b07f1dec03c225096e167668b
Steven Su <enmagic.rss@gmail.com>
2023-09-12 22:12:31 +0800

    calculate average length

--- read.py
+++ read.py
@@ -1,5 +1,6 @@
 data = []
 count = 0
+
 with open('reviews.txt', 'r') as f:
 	for line in f:
 		data.append(line)
@@ -7,7 +8,11 @@ with open('reviews.txt', 'r') as f:
 		if count % 1000 == 0:
 			print(len(data))
 
-print(len(data))
-print(data[0])
-print('---------------------------')
-print(data[1])
+print('檔案讀取完，總共有', len(data), '筆資料')
+
+
+sum_len = 0
+for d in data:
+	sum_len += len(d)
+print('每筆留言平均長度是', sum_len/len(data))
+
