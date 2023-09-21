8e6065048887d6ce8e9cb5a6515c387ca1a43aca
Steven Su <enmagic.rss@gmail.com>
2023-09-21 22:17:40 +0800

    add word count

--- read.py
+++ read.py
@@ -5,10 +5,38 @@ with open('reviews.txt', 'r') as f:
 	for line in f:
 		data.append(line)
 		count += 1
-		if count % 1000 == 0:
+		if count % 100000 == 0:
 			print(len(data))
 
 print('檔案讀取完，總共有', len(data), '筆資料')
+
+
+print(data[0])
+
+word_count= {} 
+for d in data:
+	words = d.split()
+	for word in words:
+		if word in word_count:
+			word_count[word] += 1
+		else:
+			word_count[word] = 1
+
+for word in word_count:
+	if word_count[word] > 1000000:
+		print(word, word_count[word])
+
+while True:
+	word = input('請問你想查甚麼字: ')
+	if word == 'q':
+		break
+	if word in word_count:
+		print(word, '出現過的次數為: ', word_count[word])
+	else:
+		print('這個字沒有出現過喔')
+
+print('謝謝使用本服務')
+
 
 sum_len = 0
 for d in data:
@@ -22,3 +50,10 @@ for d in data:
 print('一共有', len(new), '筆留言長度<100')
 print(new[0])
 print(new[1])
+
+good = []
+for d in data:
+	if 'good' in d:
+		good.append(d)
+print('一共有', len(good), '筆留言有提到good')
+print(good[0])
